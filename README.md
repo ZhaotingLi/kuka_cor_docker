cfd9c3fde9d3a96c6042e8fd589a97183f847509
ZhaotingLi <zhaoting_li@outlook.com>
2025-06-26 16:55:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+## Structure
+
+kuka_cor_docker/
+├── Dockerfile
+├── README.md
+├── setup_dependencies.sh
+├── kuka_cor_dependencies/
+│   ├── kuka_fri/                # Cloned repo
+│   ├── SpaceVecAlg/
+│   ├── RBDyn/
+│   ├── mc_rbdyn_urdf/
+│   ├── corrade/
+│   └── robot_controllers/
+
+
+## Set up the dependencies
+
+bash setup_dependencies.sh
---
+++ setup_dependencies.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+
+cd kuka_cor_dependencies
+
+echo "GCC version:"
+gcc --version
+echo "G++ version:"
+g++ --version
+
+echo "Cloning KUKA FRI..."
+git clone https://gitlab.tudelft.nl/kuka-iiwa-7-cor-lab/kuka_fri.git || true
+cd kuka_fri && git checkout legacy && cd ..
+
+echo "Cloning SpaceVecAlg..."
+git clone --recursive https://github.com/costashatz/SpaceVecAlg.git || true
+
+echo "Cloning RBDyn..."
+git clone --recursive https://github.com/costashatz/RBDyn.git || true
+
+echo "Cloning mc_rbdyn_urdf..."
+git clone --recursive https://github.com/costashatz/mc_rbdyn_urdf.git || true
+
+echo "Cloning Corrade..."
+git clone https://github.com/mosra/corrade.git || true
+cd corrade && git checkout 0d149ee9f26a6e35c30b1b44f281b272397842f5 && cd ..
+
+echo "Cloning robot_controllers..."
+git clone https://github.com/epfl-lasa/robot_controllers.git || true
+
+# Optional: if you meant to clone this instead of 'robotics-toolbox-python'
+# echo "Cloning robotics-toolbox-python..."
+# git clone https://github.com/petercorke/robotics-toolbox-python.git
+
